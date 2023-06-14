e8aa7aac91cdefff5e7aa0b24cca23c707e6b3f9
Andrew Townsend <andrewotownsend@gmail.com>
2023-06-14 10:14:10 -0600

    Part 3 step 3 unfinished

--- predictor/Program.cs
+++ predictor/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter text:");
-string input = Console.ReadLine();
+Console.WriteLine("enter number:");
+string numbere = Console.ReadLine();
 
-string start = "You said:";
-Console.Write(start);
-Console.WriteLine(input);
+int num = numbere, result;
+result = num << 1;
+Console.WriteLine("The number multiplied by two is: {0}", result);
