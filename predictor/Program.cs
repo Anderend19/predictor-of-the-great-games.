592526f361a6db0cbea6c32c3c4cb248224d94b2
Andrew Townsend <andrewotownsend@gmail.com>
2023-06-17 16:06:10 -0600

    PArt 3 step 5

--- predictor/Program.cs
+++ predictor/Program.cs
@@ -1,7 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("enter number:");
-string numbere = Console.ReadLine();
-
-Double num = Double.Parse(numbere);
-Double result = num << 1;
-Console.WriteLine("The number multiplied by two is: {0}", result);
+﻿
+    using System;  
+    class Expo
+{
+    static void Main()
+    {
+        double m, n;
+        Console.WriteLine("Enter the Number : ");
+        m = double.Parse(Console.ReadLine());
+        Console.WriteLine("Give the Exponent : ");
+        n = double.Parse(Console.ReadLine());
+        double value1 = Math.Pow(m, n);
+        Console.WriteLine("Result : {0}", value1);
+        Console.ReadLine();
+    }
+}
