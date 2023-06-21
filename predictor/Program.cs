73551f319536d200635b3b0e8d247287bf6a9399
Andrew Townsend <andrewotownsend@gmail.com>
2023-06-21 13:22:30 -0600

    Part 4 step 1

--- predictor/Program.cs
+++ predictor/Program.cs
@@ -1,43 +1,15 @@
 ﻿
-    using System;
-using System.Reflection;
-
-class Expo
+internal class Program
 {
-    static void Main()
+    private static void Main(string[] args)
     {
-        int m, n, a;
-        Console.Write("Enter the Number : ");
-        m = int.Parse(Console.ReadLine());
-        Console.Write("Give the Exponent : ");
-        n = int.Parse(Console.ReadLine());
-        Console.Write("Give the Exponent : ");
-        a = int.Parse(Console.ReadLine());
-        int value1 = doublesuperfunction(m, n, a);
-        Console.WriteLine("Result : {0}", value1);
-        Console.ReadLine();
-    }
-    static int doublesuperfunction(int num, int exp, int and)
-    {
-        int answer = power(num, exp);
-        return power(answer, and);
-
-    }
-    static int superpower(int num, int exp)
-    {
-        
-        return power(num, exp) + power(exp, num);
-    }
-    static int power(int num, int exp)
-    {
-        int result = 1;
-
-        for (int i = 0; i < exp; i++)
+        using (var streamReader = File.OpenText("..\\..\\.\\..\\..\\results.csv"))
         {
-            result = result * num;
+            string data = streamReader.ReadToEnd();
+            Console.WriteLine(data);
+            //string lines = streamReader.ReadToEnd().Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            //foreach (var line in lines) ;
+            // Process line
         }
-
-        return result;
     }
 }
-  
