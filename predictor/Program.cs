44c63d511153815e7de3ed15444200c58f7760b5
Andrew Townsend <andrewotownsend@gmail.com>
2023-06-20 20:28:41 -0600

    the megapower function

--- predictor/Program.cs
+++ predictor/Program.cs
@@ -6,19 +6,26 @@ class Expo
 {
     static void Main()
     {
-        int m, n;
+        int m, n, a;
         Console.Write("Enter the Number : ");
         m = int.Parse(Console.ReadLine());
         Console.Write("Give the Exponent : ");
         n = int.Parse(Console.ReadLine());
-        int value1 = superpower(m, n);
+        Console.Write("Give the Exponent : ");
+        a = int.Parse(Console.ReadLine());
+        int value1 = doublesuperfunction(m, n, a);
         Console.WriteLine("Result : {0}", value1);
         Console.ReadLine();
     }
-    
+    static int doublesuperfunction(int num, int exp, int and)
+    {
+        int answer = power(num, exp);
+        return power(answer, and);
+
+    }
     static int superpower(int num, int exp)
     {
-
+        
         return power(num, exp) + power(exp, num);
     }
     static int power(int num, int exp)
