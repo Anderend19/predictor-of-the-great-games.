7a38c032aea99055dc0c24d4d2db10aa18748a73
Andrew Townsend <andrewotownsend@gmail.com>
2023-06-23 09:19:46 -0600

    Part 4 step 2

--- predictor/Program.cs
+++ predictor/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Data.Common;
+using System.Reflection.Metadata;
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -6,9 +9,20 @@ internal class Program
         using (var streamReader = File.OpenText("..\\..\\.\\..\\..\\results.csv"))
         {
             string data = streamReader.ReadToEnd();
-            Console.WriteLine(data);
-            //string lines = streamReader.ReadToEnd().Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            //foreach (var line in lines) ;
+
+            string[] lines = data.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+           
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string text = string.Format("{0}.{1}", 201242, lines[i]);
+                Console.WriteLine(text);
+                //Console.WriteLine(lines[i]);
+
+            }
+          
+            // foreach (var line in lines) ;
             // Process line
         }
     }
