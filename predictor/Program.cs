355a46ab15b3fdeae752876f0b2d1c5b9d5e4dfb
Andrew Townsend <andrewotownsend@gmail.com>
2023-06-24 13:04:39 -0600

    Part 4 step 2

--- predictor/Program.cs
+++ predictor/Program.cs
@@ -1,6 +1,6 @@
 ﻿
-using System.Data.Common;
-using System.Reflection.Metadata;
+
+using System.Text.RegularExpressions;
 
 internal class Program
 {
@@ -12,16 +12,28 @@ internal class Program
 
             string[] lines = data.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
-           
 
-            for (int i = 0; i < lines.Length; i++)
+           
+            //Console.WriteLine(string.Format("{0} {1} - {2} {3}"Home_Team, Home_Score, Away_Team, Away_Score));
+
+            for (int i = 1; i < lines.Length; i++)
             {
-                string text = string.Format("{0}.{1}", 201242, lines[i]);
-                Console.WriteLine(text);
+                
+               // string text = string.Format("{0}.{1}", 1+i, lines[i]);
+                //Console.WriteLine(text);
+                string[] small = lines[i].Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(string.Format("{0} {1} - {2} {3}", small[1], small[3], small[2], small[4]));
+
+                //int a= 66;
+                //int b;
+                //string foo = "and";
+                //string bar = "rew";
+
+                //string blah =string.Format("hello {1} world {2} {0}", a, foo, bar);
                 //Console.WriteLine(lines[i]);
 
             }
-          
+
             // foreach (var line in lines) ;
             // Process line
         }
