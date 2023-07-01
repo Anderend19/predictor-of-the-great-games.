189b3d28a2e740f160f8139f3f21f35f200daff7
Andrew Townsend <andrewotownsend@gmail.com>
2023-07-01 08:21:54 -0600

    Part 4 step 5

---
+++ predictor/Parser.cs
@@ -0,0 +1,69 @@
+﻿using predictor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soccer_predictor
+{
+    internal class Parser
+    {
+        public List<Match> mMatches = new List<Match>();
+        public List<Team> mTeams = new List<Team>();
+
+        public void Parse()
+        {
+            string contents = File.ReadAllText("..\\..\\..\\..\\results.csv");
+            string[] lines = contents.Split('\n');
+
+            //Parse all the matches
+            for(int i = 1; i < lines.Length; i++)
+            {
+                if (lines[i].Length < 5)
+                {
+                    continue;
+                }
+                mMatches.Add(new Match(lines[i]));
+            }
+
+            for( int i = 0; i < mMatches.Count; i++) 
+            {
+                Team? homeTeam = mTeams.Find(x => x.Name == mMatches[i].HomeTeam);
+                if(homeTeam == null) 
+                {
+                    homeTeam = new Team(mMatches[i].HomeTeam);
+                    mTeams.Add(homeTeam);
+                }
+
+                Team? awayTeam = mTeams.Find(x => x.Name == mMatches[i].AwayTeam);
+                if (awayTeam == null)
+                {
+                    awayTeam = new Team(mMatches[i].AwayTeam);
+                    mTeams.Add(awayTeam);
+                }
+
+                if (mMatches[i].HomeScore == mMatches[i].AwayScore) 
+                {
+                    homeTeam.Ties++;
+                    awayTeam.Ties++;
+                }
+                else if (mMatches[i].HomeScore > mMatches[i].AwayScore) 
+                {
+                    homeTeam.Wins++;
+                    awayTeam.Losses++;
+                }
+                else
+                {
+                    homeTeam.Losses++;
+                    awayTeam.Wins++;
+                }
+            }
+
+            for(int i = 0; i < mTeams.Count; i++) 
+            {
+                Console.WriteLine(string.Format("{0} {1}W-{2}L-{3}T", mTeams[i].Name, mTeams[i].Wins, mTeams[i].Losses, mTeams[i].Ties));
+            }
+        }
+    }
+}
--- predictor/Program.cs
+++ predictor/Program.cs
@@ -1,39 +1,12 @@
 ﻿using predictor;
+using soccer_predictor;
 using System.CodeDom.Compiler;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        List<Match> mMatches = new List<Match>();
-
-        using (var streamReader = File.OpenText("..\\..\\.\\..\\..\\results.csv"))
-        {
-            string data = streamReader.ReadToEnd();
-
-            string[] lines = data.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-            //Step 1 create match list
-            for (int i = 1; i < lines.Length; i++)
-            {
-                mMatches.Add(new Match(lines[i]));
-            }
-        }
-
-        int maxScore = 0;
-        int maxIndex = 0;
-        //Step 2 use match list to find biggest home score
-        for (int i = 1; i < mMatches.Count; i++)
-        {
-            if (maxScore < mMatches[i].HomeScore)
-            {
-                maxScore = mMatches[i].HomeScore;
-                maxIndex = i;
-            }
-        }
-
-        string text = (string.Format("{0} {1}", 1 + maxIndex, mMatches[maxIndex].Raw));
-        Console.WriteLine(text);
-
+        Parser parser = new Parser();
+        parser.Parse();
     }
 }
