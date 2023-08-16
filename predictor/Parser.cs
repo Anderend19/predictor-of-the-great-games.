c68ae31579c6d64b4ead1a56ce64b4d166e7e08d
Andrew Townsend <andrewotownsend@gmail.com>
2023-08-16 15:16:54 -0600

    number to binary

--- predictor/Parser.cs
+++ predictor/Parser.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.VisualBasic;
 using System;
+using System.Numerics;
 using System.Reflection;
 
 class Expo
@@ -8,74 +9,10 @@ class Expo
     static void Main()
     {
         string equation;
-        Console.Write("equation : ");
+        Console.Write("binary : ");
         equation = Console.ReadLine();
-        string[] line = equation.Split(" (".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-        string[] lines = equation.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-        int number = int.Parse(lines[0]);
-        int numberr = int.Parse(lines[2]);
-        int numberst = int.Parse(lines[4]);
-        int numbers = 0;
-        
-
-        if (lines[1] == "+")
-        {
-            numbers = number + numberr;
-        }
-        if (lines[1] == "-")
-        {
-            numbers = number - numberr;
-        }
-        if (lines[1] == "*")
-        {
-            numbers = number * numberr;
-        }
-        if (lines[1] == "/")
-        {
-            numbers = number / numberr;
-        }
-        if (lines[1] == "^")
-        {
-           for(int i = 0; i < numberr; i++)
-            {
-                if(i == 0)
-                {
-                    numbers = number;
-                }
-                if(i > 0)
-                {
-                    numbers = numbers * number;
-                }
-            }
-        }
-        if (lines[3] == "+")
-        {
-            numbers = numbers + numberst;
-        }
-        if (lines[3] == "-")
-        {
-            numbers = numbers - numberst;
-        }
-        if (lines[3] == "*")
-        {
-            numbers = numbers * numberst;
-        }
-        if (lines[3] == "/")
-        {
-            numbers = numbers / numberst;
-        }
-        if (lines[3] == "^")
-        {
-            ijm8y fyc ghbnt help = numbers;
-            for (int i = 0; i < numberst; i++)
-            {
-                
-                if (i > 0)
-                {
-                    numbers = numbers * help;
-                }
-            }
-        }
-        Console.Write(numbers);
+        Int64 equat = int.Parse(equation);
+        string binary = Convert.ToString(equat, 10);
+        Console.Write(binary);
     }
 }
